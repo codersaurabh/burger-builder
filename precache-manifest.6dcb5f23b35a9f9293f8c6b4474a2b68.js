7cd304748565c0114f34ee5f964c4e655f1b6a13
Saurabh Kumar <devilsaurabh99@gmail.com>
2019-05-25 17:26:44 +0530

    initial build

---
+++ precache-manifest.6dcb5f23b35a9f9293f8c6b4474a2b68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff559c095f8fd95f6dee1e1f945a68c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93e2d11d298b22fbfefa",
+    "url": "/static/js/2.7a2a7a92.chunk.js"
+  },
+  {
+    "revision": "095ee76966d81ad90d39",
+    "url": "/static/js/main.697f076c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "20487f358ecac1b2b3a88560051f924b",
+    "url": "/static/media/background-burgers-home-top.20487f35.jpg"
+  },
+  {
+    "revision": "377c3b7660e2807e78a9503ac2253d71",
+    "url": "/static/media/signup-bg-1.377c3b76.jpg"
+  }
+]);
